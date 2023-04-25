a3ca315a17ae86d78f3d82ab4599557f6316cdb9
Марат Зиннатуллин <maratzinnatullin@MacBook-Air-Marat.local>
2023-04-25 12:41:41 +0300

    add themeSwitcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,14 @@
 import { AppRouter } from "app/providers/router";
 import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "./providers/ThemeProvider/lib/useTheme";
 import "./styles/index.scss";
 import { Navbar } from "widgets/Navbar";
+import { useTheme } from "app/providers/ThemeProvider";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
-
+  const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-      <button onClick={toggleTheme}>Toggle</button>
       <AppRouter />
     </div>
   );
